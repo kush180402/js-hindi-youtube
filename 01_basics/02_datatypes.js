fc31b8d96061f74ffe58838aa03d97511d6f7daf
kush180402 <kushallsahu1316@gmail.com>
2024-08-02 19:13:30 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict"; //treat all JS code as newer version.//
+
+//alert(3+3)// we are using nodejs, not browser
+
+console.log("3+3")
+//code readability should be high//
+console.log("hitesh")
+
+let name ="hitesh" //string data type//
+let age =18     // number data ype//
+let isLoggedIn = false // boolean//
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+
+
+// object
+
+console.log(typeof "hitesh");
+console.log(typeof null); //object//
+
+
