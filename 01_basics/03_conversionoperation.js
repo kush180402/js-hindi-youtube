440670f195b6bfe954b673e8155bbb979071b4bb
kush180402 <kushallsahu1316@gmail.com>
2024-08-03 17:46:30 +0000

    operations

--- 01_basics/03_conversionoperation.js
+++ 01_basics/03_conversionoperation.js
@@ -26,3 +26,52 @@ let stringNumber = String(someNumber)
 console.log(stringNumber);
 console.log(typeof stringNumber);
 
+
+//*********************operations*******************//
+
+
+
+let value = 3
+let  negValue = -value
+console.log(negValue);
+
+
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2*2);
+//console.log(2**2);
+//console.log(2/2);
+//console.log(2%2);
+
+let str1 = "hello"
+let str2 = " hitesh"
+
+let str3 = str1 + str2 // this is the simple but.
+console.log(str3);
+
+// problem arieses in this types of questions.
+
+console.log("1"+2); // 12
+console.log(1+"2"); // 12 
+
+console.log("1" + 2 + 2) // 122   // string first me hai toh subko string hi lega
+console.log(1 + 2 + "2") //32  // jo first me hoga oe lega
+
+ let num1, num2, num3
+
+ num1 = num2 = num3 = 2+2
+
+ let gameCounter = 100
+ ++gameCounter;
+ console.log(gameCounter);  //google prefix and postfix js mdn
+
+ //https://tc39.es/ecma262/multipage/abstract-operations
+
+
+
+
+
+
+
+
+
