d947db24b8d821656b0e476feb1029f8126ed5ec
kush180402 <kushallsahu1316@gmail.com>
2024-08-03 19:29:52 +0000

    comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,25 @@
+//console.log(2 > 1);
+//console.log(2 >= 1);    // This types of comparision is simple but.
+//console.log(2 < 1);
+//console.log(2 = 1);
+//console.log(2 != 1);
+
+
+//console.log("2">   1);
+//console.log("02" > 1);
+
+console.log(null > 0);  // false
+console.log(null == 0); // false
+console.log(null >= 0); // true
+                                        //confusing compare
+
+console.log(undefined == 0); //  false
+console.log(undefined > 0);  // false
+console.log(undefined < 0);  // false
+
+//=== strick check
+
+console.log("2" === 2);  // false
+
+
+
