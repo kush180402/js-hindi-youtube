f7e35fa81f4667d056b95b9398b8d8091c10f47c
kush180402 <kushallsahu1316@gmail.com>
2024-08-05 13:17:42 +0000

    datatypes-summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,43 @@
+// Two types of datatypes
+// primitives and non-primitives
+
+// 7 Types : String, Number, Boolean, Null, undefined, Symbol, BigInt.
+
+
+// Reference (Non primitive)
+
+// Array, Object, Function.
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outSideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+
+const bigNumber = 9743298742397n  //  for bigInt
+
+
+const heros = ["shaktiman", "naagraj", "doga"]
+let myObj ={
+
+    name: "hitesh",
+    age: 22,
+}   
+
+const myFunction = function(){
+    console.log("hello world")
+}
+
+console.log(typeof bigNumber);
+console.log(typeof outSideTemp);
+
+// non primitive written type object
+
+
