c2c30518b779715c2544d1321e189d20b98a5639
kush180402 <kushallsahu1316@gmail.com>
2024-08-05 19:24:11 +0000

    dataypes-summary

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -34,10 +34,60 @@ let myObj ={
 const myFunction = function(){
     console.log("hello world")
 }
-
-console.log(typeof bigNumber);
+                                     //link for study in details//
+console.log(typeof bigNumber);   //https://262.ecma-international.org/5.1/#sec-11.4.3//
 console.log(typeof outSideTemp);
 
+// undefined => "undefined"
+// null => "object"
+// boolean => "boolean"
+// number => "number"
+// string => "string"
+// object (native and does not implement [[call]]) => "object"
+// object (native or host and does implement [[call]]) => "funcion"
+// object ( host and does not implement [[call]]) => implementation- defined except may not be "undefined", "boolean","number", "stringh".
+
+
 // non primitive written type object
+
+
+
+//**************************************//
+
+// stact(Primitive),   heap(non-primitive)
+   //copy                   //reference
+
+
+let myYoutubename = "hiteshchaudharydotcom"
+
+let anothername = myYoutubename
+anothername ="chaiaurcode"
+
+console.log(myYoutubename);
+console.log(anothername);
+
+
+let userOne ={
+
+    email: "user@google.com",
+    upi: "user@ybl",
+}
+
+let userTwo = userOne
+
+userTwo.email = "hitesh@googel.com" 
+userTwo.upi ="xyz"
+
+console.log(userOne.email);
+console.log(userTwo.email);
+console.log(userTwo.upi);
+console.log(userOne.upi);
+
+
+
+
+
+
+   
 
 
