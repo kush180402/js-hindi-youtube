022eefd0012c1930d9504960575fd521a7d27361
kush180402 <kushallsahu1316@gmail.com>
2024-08-13 18:49:35 +0000

    one

---
+++ 04_control_flow/one.js
@@ -0,0 +1,78 @@
+// If-ststement.
+// code run when the condition is true.
+  
+//operator.
+
+// greater than < , 
+// lessthan than > , 
+// greater than or isequalto <=,
+// lessthan or isequalto >=,
+// equalto ==,
+// notequalto !=,
+// trniple isequalto ===, also check the type.
+// !==
+/*
+const isUserLoggedIn = true
+const temperature = 41
+
+
+if ( temperature===42){
+    console.log("yes")
+
+} else{
+    console.log("temperature is greater than 41");
+
+}
+//console.log("temperature is greater than 50");*/
+
+// another.
+/*const score = 200
+
+if(score > 100){
+    const power = " fly"
+    console.log(`User power:${power}`);
+    
+}  */
+
+
+//const balance = 1000    //impleset scope.
+//if(balance > 500) console.log("test");
+
+// another.
+/* const balance = 1000
+
+if(balance < 500) {
+    console.log("less than 500");
+
+}else if (balance < 750){
+    console.log("less than 750");
+    
+}else if (balance < 900){
+    console.log("less than 900");
+
+}else{
+    console.log("less than 1200");
+    
+
+}   */
+
+    // another.
+
+const userLoggedIn = true
+const debitCard = true
+const loggedInFromGoogle = true
+const loggedInFromEmail = false
+
+if (userLoggedIn && debitCard && 2==2){
+    console.log("allow to buy course");
+    
+}
+if(loggedInFromGoogle || loggedInFromEmail ){
+    console.log("user logged in");
+    
+}
+
+
+
+
+
