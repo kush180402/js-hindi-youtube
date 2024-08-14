7c7daa52df0ea120860b3d2b3d84a6ca040c719c
kush180402 <kushallsahu1316@gmail.com>
2024-08-14 17:33:59 +0000

    do

---
+++ 05_iterations/two.js
@@ -0,0 +1,29 @@
+
+let index = 0
+while (index <= 10) {
+    console.log(`Value of index is ${index}`);
+    index = index + 2
+
+
+}
+
+let myArray = ["flash","batman","superman"]
+
+let arr = 0
+while (arr < myArray.length) {
+    console.log(`value is ${myArray[arr]}`);
+    arr = arr + 1
+    
+    
+}
+
+// do while   // in this do whiloe loop kaam pahale hota hai conditions baad me check hoti hai.
+
+
+let score = 11
+do {
+    console.log(`score is ${score}`);
+    score++
+    
+    
+} while (score <= 10);
