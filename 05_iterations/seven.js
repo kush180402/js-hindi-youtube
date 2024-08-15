c46a966727c2fa584a19e674306a79de5ad6edf4
kush180402 <kushallsahu1316@gmail.com>
2024-08-15 11:56:50 +0000

    shopping

---
+++ 05_iterations/seven.js
@@ -0,0 +1,27 @@
+const myNumbers = [1,2,3,4,5,6,7,8,9,10]
+
+
+//const newNums = myNumbers.map( (num) => num + 10)
+//console.log(newNums);
+
+//or
+//through scope. scope me return likhna jarurib hota hai.
+
+//const newNums = myNumbers.map((num)  =>{return num + 10})
+//console.log(newNums);
+
+
+//channing.
+
+
+const newNums = myNumbers
+                 .map((num) => num * 10 )
+                 .map((num) => num + 1 )
+                 .filter((num) => num >= 40 )
+
+//console.log(newNums);
+
+
+
+
+
